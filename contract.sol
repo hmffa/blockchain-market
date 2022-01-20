274f38a23cf2532db0966821b187c9eb06ef21c7
hmffa <hmffam@gmail.com>
2022-01-21 01:09:59 +0300

    Update contract.sol
    Added seller function

--- contract.sol
+++ contract.sol
@@ -1,20 +1,48 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.4.20;
 
 contract MyContract {
 
-
-struct offerModel {
-    address sellerWalletAddress;
-    string sellerName;
-    uint itemCount;
-    string itemName;
-    string itemSpecification;
-    uint itemPrice;
+    uint public numberOfOffer;
+    address[] public buyersAddresses; // stores the addresses of buyers
 
 
-}
+    struct offerModel {
+        address sellerWalletAddress;
+        string sellerName;
+        uint itemCount;
+        string itemName;
+        string itemSpecification;
+        uint itemPrice;
+
+    }
+
+    // we map a unique ID for each offer
+    mapping (uint => offerModel) public offers;
 
 
-function createOffer() public payable {}
+    function createOffer(string _sellerName , uint _itemCount, string _itemName , string _itemSpecification , uint _itemPrice) external {
+
+        numberOfOffer ++;
+        offers[numberOfOffer].sellerWalletAddress = msg.sender;
+        offers[numberOfOffer].sellerName = _sellerName;
+        offers[numberOfOffer].itemCount = _itemCount;
+        offers[numberOfOffer].itemName = _itemName;
+        offers[numberOfOffer].itemSpecification = _itemSpecification;
+        offers[numberOfOffer].itemPrice = _itemPrice;
+
+    }
+
+    function searchOffers (string _itemName) public view returns(string) {
+        for(uint i=0;i<numberOfOffer;i++)
+        {
+            if(keccak256(offers[i].itemName) == keccak256(_itemName)){
+                return offers[i];
+            }
+        }
+    }
+
+    // function viewOffers () view public returns(uint) {
+    //     return offers;
+    // }
 
 }
